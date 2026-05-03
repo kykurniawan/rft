e4c1a89fd87db602a50df43e6d4d31ebbd61734b
Rizky Kurniawan <55426692+kykurniawan@users.noreply.github.com>
2026-05-03 10:55:21 +0700

    basic program

--- crates/rft-basic/src/main.rs
+++ crates/rft-basic/src/main.rs
@@ -1,8 +1,32 @@
+use std::io;
+
+mod control_flow;
+mod data_type;
 mod formatting;
 mod variable;
-mod data_type;
-mod control_flow;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hi, I'm a Rust program! Please type your name:");
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+
+    println!("Hello, {}! How old are you?", name.trim());
+
+    let mut age_input = String::new();
+
+    loop {
+        age_input.clear(); // clear previous input
+        io::stdin().read_line(&mut age_input).unwrap();
+
+        if age_input.trim().parse::<u32>().is_ok() {
+            break;
+        }
+
+        println!("Please enter a valid age:");
+    }
+
+    let age: u32 = age_input.trim().parse().unwrap();
+
+    println!("Hello, {}! You are {} years old.", name.trim(), age);
 }
