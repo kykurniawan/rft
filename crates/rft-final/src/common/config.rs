a8435e959b189834af315a8277ed03603dfcc3bd
Rizky Kurniawan <55426692+kykurniawan@users.noreply.github.com>
2026-05-14 02:12:24 +0700

    config

--- crates/rft-final/src/common/config.rs
+++ crates/rft-final/src/common/config.rs
@@ -1,24 +1,5 @@
-use config::{ConfigError, Environment};
+use config::{ConfigBuilder, ConfigError, Environment, builder::DefaultState};
 use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-pub struct Config {
-    pub tracing: Tracing,
-    pub database: Database,
-}
-
-impl Config {
-    pub fn new() -> Result<Self, ConfigError> {
-        let cfg = config::Config::builder()
-            .add_source(Environment::default().ignore_empty(true).separator("__"))
-            .set_default("tracing.directory", "log")?
-            .set_default("tracing.filename_suffix", "application.log")?
-            .build()?;
-
-        cfg.try_deserialize()
-    }
-}
 
 #[derive(Debug, Deserialize, Clone)]
 #[allow(unused)]
@@ -31,4 +12,42 @@ pub struct Tracing {
 #[allow(unused)]
 pub struct Database {
     pub connection_string: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[allow(unused)]
+pub struct Server {
+    pub host: String,
+    pub port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct Config {
+    pub tracing: Tracing,
+    pub database: Database,
+    pub server: Server,
+}
+
+impl Config {
+    pub fn new() -> Result<Self, ConfigError> {
+        let cfg = Self::with_defaults(
+            config::Config::builder()
+                .add_source(Environment::default().ignore_empty(true).separator("__")),
+        )?;
+
+        cfg.build()?.try_deserialize()
+    }
+
+    fn with_defaults(
+        mut cfg: ConfigBuilder<DefaultState>,
+    ) -> Result<ConfigBuilder<DefaultState>, ConfigError> {
+        cfg = cfg
+            .set_default("tracing.directory", "logs")?
+            .set_default("tracing.filename_suffix", "application.log")?
+            .set_default("server.host", "0.0.0.0")?
+            .set_default("server.port", 3000)?;
+
+        Ok(cfg)
+    }
 }
